a7117aece339f8225e8bcd7b0c8e7a08e863cc96
Jonster5 <onlyaccelerating@gmail.com>
2023-07-24 16:47:39 -0400

    renamed src to lib

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	plugins: [dts({ insertTypesEntry: true })],
 	build: {
 		lib: {
-			entry: ['./src/main.ts', './src/math.ts'],
+			entry: ['./lib/main.ts', './lib/math.ts'],
 			name: 'raxis',
 			fileName: (f, n) => `raxis-${n}.${f === 'cjs' ? f : 'js'}`,
 			formats: ['es', 'cjs'],
